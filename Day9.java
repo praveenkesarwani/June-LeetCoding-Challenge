18907f6892e1a5950b321507a3935af2c25ef459
Praveen <pk99411@gmail.com>
2020-06-10 09:15:33 +0530

    Is Subsequence

---
+++ Day9.java
@@ -0,0 +1,14 @@
+//Is Subsequence
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0)
+            return true;
+        int i = 0;
+        for(char c:t.toCharArray()){
+            if(i < s.length() && c == s.charAt(i))
+                i++;
+        }
+        return i == s.length();
+    }
+}
+//Time Complexity : O(N) , N is the length of string t
